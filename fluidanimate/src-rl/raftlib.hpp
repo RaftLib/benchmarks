b8f8b2a23ad5482ca55bd3283c5fc2fe03bcb255
James Wood <woodjamesdee@gmail.com>
2020-07-20 14:29:13 -0400

    Added kernels for functions that do not use mutex locks.

--- fluidanimate/src-rl/raftlib.hpp
+++ fluidanimate/src-rl/raftlib.hpp
@@ -77,6 +77,8 @@ private:
     int* g_cnumPars;
     int g_ny;
     int g_nx;
+
+
 public:
     RebuildGridMTKernel(Grid* grids, Cell* cells, Cell** last_cells, int* cnumPars, int ny, int nx)
         : g_Grids(grids), g_Cells(cells), g_last_cells(last_cells), g_cnumPars(cnumPars), g_ny(ny), g_nx(nx)
@@ -94,5 +96,292 @@ public:
         return raft::proceed;
     }
 };
+
+class InitDensitiesAndForcesMTKernel : public raft::kernel
+{
+private:
+    Grid* g_Grids;
+    Cell* g_Cells;
+    int* g_cnumPars;
+    int g_ny;
+    int g_nx;
+public:
+    InitDensitiesAndForcesMTKernel(Grid* grids, Cell* cells, int* cnumPars, int ny, int nx)
+        : g_Grids(grids), g_Cells(cells), g_cnumPars(cnumPars), g_ny(ny), g_nx(nx)
+    {
+        input.addPort<int>("in");
+        output.addPort<int>("out");
+    }
+
+    virtual raft::kstatus run()
+    {
+        int tid = input["in"].peek<int>();
+        for(int iz = g_Grids[tid].sz; iz < g_Grids[tid].ez; ++iz)
+            for(int iy = g_Grids[tid].sy; iy < g_Grids[tid].ey; ++iy)
+            for(int ix = g_Grids[tid].sx; ix < g_Grids[tid].ex; ++ix)
+            {
+                int index = (iz*g_ny + iy)*g_nx + ix;
+                Cell *cell = &g_Cells[index];
+                int np = g_cnumPars[index];
+                for(int j = 0; j < np; ++j)
+                {
+                    cell->density[j % PARTICLES_PER_CELL] = 0.0;
+                    cell->a[j % PARTICLES_PER_CELL] = externalAcceleration;
+                    //move pointer to next cell in list if end of array is reached
+                    if(j % PARTICLES_PER_CELL == PARTICLES_PER_CELL-1) {
+                        cell = cell->next;
+                }
+                }
+            }
+
+        output["out"].push<int>(tid);
+        input["in"].recycle();
+
+        return raft::proceed;
+    }
+};
+
+class ProcessCollisionsMTKernel : public raft::kernel
+{
+private:
+    Grid* g_Grids;
+    Cell* g_Cells;
+    int* g_cnumPars;
+    int g_ny;
+    int g_nx;
+public:
+    ProcessCollisionsMTKernel(Grid* grids, Cell* cells, int* cnumPars, int ny, int nx)
+        : g_Grids(grids), g_Cells(cells), g_cnumPars(cnumPars), g_ny(ny), g_nx(nx)
+    {
+        input.addPort<int>("in");
+        output.addPort<int>("out");
+    }
+
+    virtual raft::kstatus run()
+    {
+        int tid = input["in"].peek<int>();
+        for(int iz = g_Grids[tid].sz; iz < g_Grids[tid].ez; ++iz)
+        {
+            for(int iy = g_Grids[tid].sy; iy < g_Grids[tid].ey; ++iy)
+            {
+                for(int ix = g_Grids[tid].sx; ix < g_Grids[tid].ex; ++ix)
+                {
+                    if(!((ix==0)||(iy==0)||(iz==0)||(ix==(nx-1))||(iy==(ny-1))==(iz==(nz-1))))
+                        continue;	// not on domain wall
+                    int index = (iz*g_ny + iy)*g_nx + ix;
+                    Cell *cell = &g_Cells[index];
+                    int np = g_cnumPars[index];
+                    for(int j = 0; j < np; ++j)
+                    {
+                        int ji = j % PARTICLES_PER_CELL;
+                        Vec3 pos = cell->p[ji] + cell->hv[ji] * timeStep;
+
+                        if(ix==0)
+                        {
+                            fptype diff = parSize - (pos.x - domainMin.x);
+                            if(diff > epsilon)
+                            cell->a[ji].x += stiffnessCollisions*diff - damping*cell->v[ji].x;
+                        }
+                        if(ix==(nx-1))
+                        {
+                            fptype diff = parSize - (domainMax.x - pos.x);
+                            if(diff > epsilon)
+                            cell->a[ji].x -= stiffnessCollisions*diff + damping*cell->v[ji].x;
+                        }
+                        if(iy==0)
+                        {
+                            fptype diff = parSize - (pos.y - domainMin.y);
+                            if(diff > epsilon)
+                            cell->a[ji].y += stiffnessCollisions*diff - damping*cell->v[ji].y;
+                        }
+                        if(iy==(ny-1))
+                        {
+                            fptype diff = parSize - (domainMax.y - pos.y);
+                            if(diff > epsilon)
+                            cell->a[ji].y -= stiffnessCollisions*diff + damping*cell->v[ji].y;
+                        }
+                        if(iz==0)
+                        {
+                            fptype diff = parSize - (pos.z - domainMin.z);
+                            if(diff > epsilon)
+                            cell->a[ji].z += stiffnessCollisions*diff - damping*cell->v[ji].z;
+                        }
+                        if(iz==(nz-1))
+                        {
+                            fptype diff = parSize - (domainMax.z - pos.z);
+                            if(diff > epsilon)
+                            cell->a[ji].z -= stiffnessCollisions*diff + damping*cell->v[ji].z;
+                        }
+                        //move pointer to next cell in list if end of array is reached
+                        if(ji == PARTICLES_PER_CELL-1) 
+                            cell = cell->next;
+                    }
+                }
+            }
+        }
+        output["out"].push<int>(tid);
+        input["in"].recycle();
+
+        return raft::proceed;
+    }
+};
+
+class ProcessCollisions2MTKernel : public raft::kernel
+{
+private:
+    Grid* g_Grids;
+    Cell* g_Cells;
+    int* g_cnumPars;
+    int g_ny;
+    int g_nx;
+public:
+    ProcessCollisions2MTKernel(Grid* grids, Cell* cells, int* cnumPars, int ny, int nx)
+        : g_Grids(grids), g_Cells(cells), g_cnumPars(cnumPars), g_ny(ny), g_nx(nx)
+    {
+        input.addPort<int>("in");
+        output.addPort<int>("out");
+    }
+
+    virtual raft::kstatus run()
+    {
+        int tid = input["in"].peek<int>();
+        for(int iz = g_Grids[tid].sz; iz < g_Grids[tid].ez; ++iz)
+        {
+            for(int iy = g_Grids[tid].sy; iy < g_Grids[tid].ey; ++iy)
+            {
+                for(int ix = g_Grids[tid].sx; ix < g_Grids[tid].ex; ++ix)
+                {
+                    int index = (iz*g_ny + iy)*g_nx + ix;
+                    Cell *cell = &g_Cells[index];
+                    int np = g_cnumPars[index];
+                    for(int j = 0; j < np; ++j)
+                    {
+                        int ji = j % PARTICLES_PER_CELL;
+                        Vec3 pos = cell->p[ji];
+
+                        if(ix==0)
+                        {
+                            fptype diff = pos.x - domainMin.x;
+                            if(diff < Zero)
+                            {
+                                cell->p[ji].x = domainMin.x - diff;
+                                cell->v[ji].x = -cell->v[ji].x;
+                                cell->hv[ji].x = -cell->hv[ji].x;
+                            }
+                        }
+                        if(ix==(nx-1))
+                        {
+                            fptype diff = domainMax.x - pos.x;
+                            if(diff < Zero)
+                            {
+                                cell->p[ji].x = domainMax.x + diff;
+                                cell->v[ji].x = -cell->v[ji].x;
+                                cell->hv[ji].x = -cell->hv[ji].x;
+                            }
+                        }
+                        if(iy==0)
+                        {
+                            fptype diff = pos.y - domainMin.y;
+                            if(diff < Zero)
+                            {
+                                cell->p[ji].y = domainMin.y - diff;
+                                cell->v[ji].y = -cell->v[ji].y;
+                                cell->hv[ji].y = -cell->hv[ji].y;
+                            }
+                        }
+                        if(iy==(ny-1))
+                        {
+                            fptype diff = domainMax.y - pos.y;
+                            if(diff < Zero)
+                            {
+                                cell->p[ji].y = domainMax.y + diff;
+                                cell->v[ji].y = -cell->v[ji].y;
+                                cell->hv[ji].y = -cell->hv[ji].y;
+                            }
+                        }
+                        if(iz==0)
+                        {
+                            fptype diff = pos.z - domainMin.z;
+                            if(diff < Zero)
+                            {
+                                cell->p[ji].z = domainMin.z - diff;
+                                cell->v[ji].z = -cell->v[ji].z;
+                                cell->hv[ji].z = -cell->hv[ji].z;
+                            }
+                        }
+                        if(iz==(nz-1))
+                        {
+                            fptype diff = domainMax.z - pos.z;
+                            if(diff < Zero)
+                            {
+                                cell->p[ji].z = domainMax.z + diff;
+                                cell->v[ji].z = -cell->v[ji].z;
+                                cell->hv[ji].z = -cell->hv[ji].z;
+                            }
+                        }
+                        //move pointer to next cell in list if end of array is reached
+                        if(ji == PARTICLES_PER_CELL-1)
+                        cell = cell->next;
+                    }
+                }
+            }
+        }
+        output["out"].push<int>(tid);
+        input["in"].recycle();
+
+        return raft::proceed;
+    }
+};
+
+class AdvanceParticlesMTKernel : public raft::kernel
+{
+private:
+    Grid* g_Grids;
+    Cell* g_Cells;
+    int* g_cnumPars;
+    int g_ny;
+    int g_nx;
+public:
+    AdvanceParticlesMTKernel(Grid* grids, Cell* cells, int* cnumPars, int ny, int nx)
+        : g_Grids(grids), g_Cells(cells), g_cnumPars(cnumPars), g_ny(ny), g_nx(nx)
+    {
+        input.addPort<int>("in");
+        output.addPort<int>("out");
+    }
+
+    virtual raft::kstatus run()
+    {
+        int tid = input["in"].peek<int>();
+
+        for(int iz = g_Grids[tid].sz; iz < g_Grids[tid].ez; ++iz)
+            for(int iy = g_Grids[tid].sy; iy < g_Grids[tid].ey; ++iy)
+                for(int ix = g_Grids[tid].sx; ix < g_Grids[tid].ex; ++ix)
+                {
+                    int index = (iz*ny + iy)*nx + ix;
+                    Cell *cell = &g_Cells[index];
+                    int np = g_cnumPars[index];
+                    for(int j = 0; j < np; ++j)
+                    {
+                        Vec3 v_half = cell->hv[j % PARTICLES_PER_CELL] + cell->a[j % PARTICLES_PER_CELL]*timeStep;
+                        cell->p[j % PARTICLES_PER_CELL] += v_half * timeStep;
+                        cell->v[j % PARTICLES_PER_CELL] = cell->hv[j % PARTICLES_PER_CELL] + v_half;
+                        cell->v[j % PARTICLES_PER_CELL] *= 0.5;
+                        cell->hv[j % PARTICLES_PER_CELL] = v_half;
+
+                        //move pointer to next cell in list if end of array is reached
+                        if(j % PARTICLES_PER_CELL == PARTICLES_PER_CELL-1)
+                            cell = cell->next;
+                    }
+                }
+
+        output["out"].push<int>(tid);
+        input["in"].recycle();
+
+        return raft::proceed;
+    }
+};
+
+
+
 
 #endif
