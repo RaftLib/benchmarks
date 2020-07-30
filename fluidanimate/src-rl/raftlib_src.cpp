49d207b55f2458571ad267bed4392df7472ed321
James Wood <woodjamesdee@gmail.com>
2020-07-29 20:08:06 -0400

    Added README data and copyright notices.

--- fluidanimate/src-rl/raftlib_src.cpp
+++ fluidanimate/src-rl/raftlib_src.cpp
@@ -2,6 +2,26 @@
 //Modified by Christian Fensch
 // RaftLib implementation provided by James Wood
 
+/**
+ * raftlib_src.cpp
+ * @author: James Wood
+ * @version: Thu July 29 18:05:00 2020
+ *
+ * Copyright 2020 Jonathan Beard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 
 #include <cstdlib>
 #include <cstring>
