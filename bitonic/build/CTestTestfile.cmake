4f744ef6e5a5863e54d2f713bbd2e1ff5fdc3b3e
Jonathan Beard <jonathan.c.beard@gmail.com>
2020-07-31 12:46:51 -0500

    initial commit with all additions

--- bitonic/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jonathan/GIT_RPO/benchmarks/bitonic
-# Build directory: /home/jonathan/GIT_RPO/benchmarks/bitonic/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("testsuite")
--- bitonic/build/testsuite/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jonathan/GIT_RPO/benchmarks/bitonic/testsuite
-# Build directory: /home/jonathan/GIT_RPO/benchmarks/bitonic/build/testsuite
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
